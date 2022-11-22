5895b158feef90c504cdf5f0aa3487cd3348b8d6
abel <abrianvil@gmail.com>
2022-11-22 18:49:14 -0500

    dev branch creation

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,8 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
+    first_name=db.Column(db.String(40), nullable=False)
+    last_name=db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
@@ -28,6 +30,8 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'firstName':self.first_name,
+            'lastName':self.last_name,
             'username': self.username,
             'email': self.email
         }
