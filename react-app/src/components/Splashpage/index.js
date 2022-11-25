ff624c3efc2f63c2797ad7dfc1efb45dbc2905a5
abel <abrianvil@gmail.com>
2022-11-25 01:16:28 -0500

    basic css for splash page/login and sign up component strated

--- react-app/src/components/Splashpage/index.js
+++ react-app/src/components/Splashpage/index.js
@@ -1,40 +1,92 @@
 import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import LoginForm from '../auth/LoginForm'
-import img1 from '../../images'
-import debitCard from '../../images'
-import assets_nav_spend2 from '../../images'
-import assets_nav_spend from '../../images'
+import bank from '../../Images/img1.webp'
+import debitCard from '../../Images/debitCard.png'
+import assets_nav_spend2 from '../../Images/assets_nav_spend2.png'
+// import assets_nav_spend from '../../Images/assets_nav_spend.png'
+import NavBar from '../NavBar'
+import './index.css'
 
 
 function SplashPage() {
-
+const history=useHistory()
+    function buttonClick(e){
+        e.preventDefault()
+        history.push('/login')
+    }
 
     return (
-        <div className='landing-page'>
-            <div className='nav-bar'></div>
-            <div className='splash-body'>
-                <div>Do more with your money</div>
-                <div className='content-box'>
-                    <div className='picture-box'>
-                        <div className='photo'>
-                            <img src={img1} alt='' />
-                        </div>
+        <div className='page'>
+            <div className='nav-bar'>
+                <div>Home</div>
+                <button onClick={buttonClick} className='user-login'>
+                    <i className="fa-solid fa-bars"></i>
+                </button>
 
-                        <div className='photo'>
-                            <img src={debitCard} alt='' />
-                        </div>
+            </div>
+            <div className='landing-page'>
+                <div className='splash-body'>
+                    <div className='title'>Do more with your money</div>
+                    <div className='content-box'>
+                        <div className='picture-box'>
+                            <div className='photo'>
+                                <img src={bank} alt='' />
+                                <div className='title-2'>Bank</div>
+                                <div className='tag'>Faster, simpler banking</div>
+                            </div>
 
-                        <div className='photo'>
+                            <div className='photo'>
+                                <img src={debitCard} alt='' />
+                                <div className='title-2'>Spend</div>
+                                <div className='tag'>Save on everyday spending</div>
+                            </div>
+
+                            {/* <div className='photo'>
                             <img src={assets_nav_spend} alt='' />
-                        </div>
+                        </div> */}
 
-                        <div className='photo'>
-                            <img src={assets_nav_spend2} alt='' />
+                            <div className='photo'>
+                                <img src={assets_nav_spend2} alt='' />
+                                <div className='title-2'>Send</div>
+                                <div className='tag'>Pay and get paid instantly</div>
+                            </div>
+                        </div>
+                        <div className='links-box'>
+                            <div>Support</div>
+                            <div>links</div>
+                            <div>
+                                Stay in touch
+                                <Link
+                                    className='contact-button'
+                                    to={{
+                                        pathname:
+                                            'https://www.linkedin.com/in/abel-brianvil-ba4320170/',
+                                    }}
+                                    target='_blank'
+                                >
+                                    <div className='icon'>
+                                        <i className='fa-brands fa-linkedin'></i>
+                                    </div>
+                                </Link>
+
+                                <Link
+                                    className='contact-button'
+                                    to={{ pathname: 'https://github.com/abrianvil' }}
+                                    target='_blank'
+                                >
+                                    <div className='icon'>
+                                        <i className='fa-brands fa-square-github' />
+                                    </div>
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                    <div className='links-box'></div>
                 </div>
             </div>
         </div>
     )
 }
+
+
+export default SplashPage
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import SignUpForm from './SignUpForm';
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [form, setForm] = useState(true)
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
@@ -31,34 +33,56 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div className='login-signup-div'>
+      {form && (
+        <div>
+          <form onSubmit={onLogin}>
+            <div>
+              {errors.map((error, ind) => (
+                <div key={ind}>{error}</div>
+              ))}
+            </div>
+            <div>
+              <label htmlFor='email'>Email</label>
+              <input
+                name='email'
+                type='text'
+                placeholder='Email'
+                value={email}
+                onChange={updateEmail}
+              />
+            </div>
+            <div>
+              <label htmlFor='password'>Password</label>
+              <input
+                name='password'
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={updatePassword}
+              />
+              <button type='submit'>Login</button>
+            </div>
+          </form>
+
+          <div className='info'>
+                <h3>Don't have an account yet</h3>
+                <div>Sign up and start Banking with us</div>
+                <button onClick={()=>setForm(false)}>Sign Up</button>
+          </div>
+        </div>
+      )}
+      {!form && (
+        <>
+        <SignUpForm />
+        <div className='info'>
+                <h3>Already have an account</h3>
+                <div>Sign up and start Banking with us</div>
+                <button onClick={()=>setForm(true)}>Login</button>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
