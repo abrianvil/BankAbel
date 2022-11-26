8566b71150df5857044d572fdf5ac6c4270e746d
abel <abrianvil@gmail.com>
2022-11-25 23:56:32 -0500

    created accounts redux store/more testing needed - added code so on logout you are redirected to splash page

---
+++ react-app/src/components/landingPage/index.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import './index.css'
+
+
+
+
+
+const LandingPage = () => {
+
+
+    return (
+        <div className='main-page'>
+            <div className='navigation-bar'>
+
+            </div>
+
+            <div className='content-display-box'>
+
+            </div>
+
+            <div className='footer'>
+
+            </div>
+        </div>
+    )
+}
+
+
+export default LandingPage
