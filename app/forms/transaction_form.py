cbd2a8714b34455fd0046da0c6b0de69cd4add2f
abel <abrianvil@gmail.com>
2022-11-26 14:47:23 -0500

    added wallet route in the backend

--- app/forms/transaction_form.py
+++ app/forms/transaction_form.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, DateField,IntegerField
 from wtforms.validators import DataRequired
-from app.models import Transaction
 
 
 class New_transaction(FlaskForm):
@@ -10,4 +9,3 @@ class New_transaction(FlaskForm):
     due_date=DateField('Due Date')
     status=StringField('status', default='Pending')
     receiver_id=IntegerField('receiver', validators=[DataRequired()])
-    
