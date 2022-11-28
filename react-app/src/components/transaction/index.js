d3ba0113259fb7020967532ba03454a92c8d70b8
abel <abrianvil@gmail.com>
2022-11-28 00:29:41 -0500

    created FE routes for activity/account/wallet components

--- react-app/src/components/transaction/index.js
+++ react-app/src/components/transaction/index.js
@@ -3,13 +3,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllAccounts } from '../../store/account';
 import { getWallet } from '../../store/wallet';
 import { getAllTransactions } from '../../store/transaction';
+import LogoutButton from '../auth/LogoutButton';
 import './index.css'
+import { useHistory } from 'react-router-dom';
 
 
 
 
 const TransactionComp = () => {
     const dispatch = useDispatch()
+    const history=useHistory()
 
     const user = useSelector(state => state.session.user)
     const wallet = useSelector(state => state.wallet.wallet)
@@ -29,9 +32,22 @@ const TransactionComp = () => {
             setUsers(res);
         }
         fetchData();
-    }, [dispatch]);
+    }, []);
 
-    console.log('this is the user list', users)
+    const clickUser=()=>{
+        history.push('/dashboard')
+    }
+    const clickWallet = () => {
+        history.push('/wallet')
+    }
+
+    const clickAccount = () => {
+        history.push('/account')
+    }
+
+    const clickActivity = () => {
+        history.push('/activity')
+    }
 
     useEffect(() => {
         dispatch(getAllAccounts())
@@ -42,23 +58,64 @@ const TransactionComp = () => {
     console.log(users[2])
 
     return (
-        <div className='trans'>
-            <div id='header'>
-                TRANSACTIONS
-            </div>
-            <div className='transaction-box'>
-                {transactions.map(transaction => (
-                    <div className='single-trans' key={transaction.id}>
-                        <div className='image'>
-                            <img src={users[transaction['receiver']].picture} />
-                        </div>
-                        <div>${transaction.amount}</div>
-                        <div>{transaction.createdAt}</div>
-                    </div>
-                ))}
 
+        <div className='main-page'>
+            <div className='navigation-bar'>
+
+            <div className='user-card' onClick={clickUser}>
+                    <div className='image' >
+                        <img src={user.picture} alt={user.id} />
+                    </div>
+                    <div>{user.firstName} {user.lastName}</div>
+                </div>
+
+                <div className='components'>
+                    <div className='wallet' onClick={clickWallet}>
+                        <i className="fa-solid fa-wallet" /> Wallet
+                    </div>
+
+                    <div className='accounts' onClick={clickAccount}>
+                        <i className="fa-solid fa-money-check-dollar" /> Accounts
+                    </div>
+
+                    <div className='activity' onClick={clickActivity}>
+                        <i className="fa-solid fa-clock-rotate-left" /> Activity
+                    </div>
+                </div>
+
+                <LogoutButton />
             </div>
+
+            <div className='content-footer'>
+                <div className='content-display-box'>
+                    <div className='trans'>
+                        <div id='header'>
+                            TRANSACTIONS
+                        </div>
+                        <div className='transaction-box'>
+                            {transactions.map(transaction => (
+                                <div className='single-trans' key={transaction.id}>
+                                    <div className='image'>
+                                        <img src={users[transaction['receiver']].picture} />
+                                    </div>
+                                    <div>${transaction.amount}</div>
+                                    <div>{transaction.createdAt}</div>
+                                </div>
+                            ))}
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className='footer'>
+                    <div className='logo'></div>
+                    {/* <div>this is for abel</div> */}
+                </div>
+            </div>
+
         </div>
+
+
     )
 
 }
